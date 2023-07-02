219b3d45d392170bde416a76946b1e0573af828f
malik <elad.malik@mail.huji.ac.il>
2023-07-02 15:43:48 +0300

    preprocess: split address to street, city and state

--- preprocessing.py
+++ preprocessing.py
@@ -31,10 +31,53 @@ def parse_numeric_cols(df):
     return df
 
 
-def build_df_from_data(save_path: str = "california_data.csv", base_data_dir:
-str = "data"):
+def parse_address(df):
+    def parse_row(row):
+        street_raw, city_raw, state_raw = row['address'].split(", ")
+
+        if 'UNIT' in street_raw:
+            street_raw = street_raw[:street_raw.index('UNIT') - 1]
+        elif 'APT' in street_raw:
+            street_raw = street_raw[:street_raw.index('APT') - 1]
+        elif 'SPACE' in street_raw:
+            street_raw = street_raw[:street_raw.index('SPACE') - 1]
+        elif '#' in street_raw:
+            street_raw = street_raw[:street_raw.index('#') - 1]
+        street = street_raw[street_raw.index(' ') + 1:]
+
+        city = city_raw
+
+        state = state_raw[:2]
+
+        return street, city, state
+
+    new_df = df.copy()
+    split_data = pd.DataFrame(list(df.apply(parse_row, axis=1)), columns=['street', 'city', 'state'])
+    new_df = new_df.drop(columns=['address'])
+    new_df = pd.concat([new_df, split_data], axis=1)
+    return new_df
+
+
+def reorder_columns(df):
+    order = [
+        'zpid',
+        'bed',
+        'bath',
+        'sqft',
+        'street',
+        'city',
+        'state',
+        'overview',
+        'price'
+    ]
+    return df[order]
+
+
+def build_df_from_data(save_path: str = "california_data.csv", base_data_dir: str = "data"):
     df = build_raw_df(base_data_dir)
     df = parse_numeric_cols(df)
+    df = parse_address(df)
+    df = reorder_columns(df)
     df.to_csv(save_path, index=False)
 
 
