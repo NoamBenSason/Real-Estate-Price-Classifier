4f7c0593fac61d3a77868ce13b6964d801ede36f
Noam Ben Sason <noam.bensason@gmail.com>
2023-07-05 19:14:15 +0300

    zero shot: added wandb, batches, 2-digit numbers option for the masking

---
+++ wandb_zero_shot.py
@@ -0,0 +1,58 @@
+import wandb
+from transformers import BertForMaskedLM, RobertaForMaskedLM, ElectraForMaskedLM
+from zero_shot import save_to_buffer, save_results, N_EXAMPLE_SAMPLES, zero_shot, BATCH_SIZE, ceil
+from preprocessing import build_df_from_data, format_dataframe
+
+MODELS_DICT = {
+    # model name: model object, mask format
+    "bert-base-uncased": (BertForMaskedLM, "[MASK]"),
+    "roberta-base": (RobertaForMaskedLM, "<mask>"),
+    "google/electra-base-generator": (ElectraForMaskedLM, "[MASK]")
+}
+
+
+def get_config():
+    sweep_config = {}
+    sweep_config['method'] = 'random'
+    sweep_config['metric'] = {'name': 'mse', 'goal': 'maximize'}
+    sweep_config['name'] = f"zero_shot"
+
+    param_dict = {
+        'model_name': {'values': ["bert-base-uncased", "roberta-base", "google/electra-base-generator"]}
+    }
+
+    sweep_config['parameters'] = param_dict
+    return sweep_config
+
+
+def wandb_zero_shot(config=None):
+    with wandb.init(config=config):
+        config = wandb.config
+        buffer = ""
+        model_name = config["model_name"]
+        model_for_lm, mask_format = MODELS_DICT[model_name]
+        train_data = format_dataframe("train_data.csv", "{overview}")
+
+        avg_losses = [0, 0, 0]
+        for i in range(0, len(train_data), BATCH_SIZE):
+            losses, truncated_formatted_sentences, true_data_completion, predicted_prices = zero_shot(
+                model_name,
+                model_for_lm,
+                mask_format,
+                train_data[i:i + BATCH_SIZE])
+            if i == 0:
+                examples = [truncated_formatted_sentences[:N_EXAMPLE_SAMPLES],
+                            predicted_prices[:N_EXAMPLE_SAMPLES],
+                            true_data_completion[:N_EXAMPLE_SAMPLES]]
+            avg_losses[0] += losses[0]
+            avg_losses[1] += losses[1]
+            avg_losses[2] += losses[2]
+            wandb.log({"r2_squared": losses[0], "mse": losses[1], "mae": losses[2]})
+        num_batches = ceil(len(train_data) / BATCH_SIZE)
+        buffer = save_to_buffer(model_name, [avg / num_batches for avg in avg_losses], examples, buffer)
+        save_results(buffer)
+
+
+if __name__ == '__main__':
+    sweep_id = wandb.sweep(get_config(), project="zero_shot", entity="selling_bat_yam")
+    wandb.agent(sweep_id, wandb_zero_shot, count=1)
--- zero_shot.py
+++ zero_shot.py
@@ -1,13 +1,14 @@
 from transformers import AutoTokenizer, BertForMaskedLM, RobertaForMaskedLM, ElectraForMaskedLM
 import torch
 from preprocessing import build_df_from_data, format_dataframe
-# import wandb
 from evaluate import load
+from math import ceil
 
-SLICE_DATA_FOR_DEBUG = 20
+SLICE_DATA_FOR_DEBUG = 3
 X_INDX_IN_TUPLE = 0
 Y_INDX_IN_TUPLE = 1
-N_EXAMPLE_SAMPLES = 20
+N_EXAMPLE_SAMPLES = 3
+BATCH_SIZE = 50
 # FORMAT_STR = "The price of this house is {mask_format}.{mask_format}$ million."
 FORMAT_STR = "The price of this house is ({mask_format}.{mask_format})$ million."
 
@@ -20,65 +21,57 @@ MODELS = [
 
 
 def predictions_to_numbers(predicted_words):
-  """
-  gets the words that the model completed a list and converts them to list of
-  numbers representing the prediction in millions
-  :param predicted_words: list of completed words by the model, length of num_samples *2
-  :return:
-  """
-  num_predictions = []
-  for i in range(0, len(predicted_words), 2):
-      num_predictions.append(float(f"{predicted_words[i]}.{predicted_words[i + 1]}".replace(" ", "")))
-  return num_predictions
+    """
+    gets the words that the model completed a list and converts them to list of
+    numbers representing the prediction in millions
+    :param predicted_words: list of completed words by the model, length of num_samples *2
+    :return:
+    """
+    num_predictions = []
+    for i in range(0, len(predicted_words), 2):
+        num_predictions.append(float(f"{predicted_words[i]}.{predicted_words[i + 1]}".replace(" ", "")))
+    return num_predictions
 
 
 def extract_data(lst, index):
+    """
+    extract only the samples or only the labels from a tuple of both
+    :param lst: list of tuples in format of (sample,label)
+    :param index: 0 for extracting samples list, 1 for extracting labels list
+    :return: samples list or labels list
+    """
+    return [item[index] for item in lst]
 
-  """
-  extract only the samples or only the labels from a tuple of both
-  :param lst: list of tuples in format of (sample,label)
-  :param index: 0 for extracting samples list, 1 for extracting labels list
-  :return: samples list or labels list
-  """
-  return [item[index] for item in lst]
+
+def get_numerical_tokens(tokenizer):
+    num_list = list(map(str, range(101)))
+    s = ' '.join(num_list)
+    numerical_ids = tokenizer.encode(s, add_special_tokens=False)
+    return numerical_ids
 
 
-# def save_results(model_name, losses, samples, predictions, labels):
+def save_to_buffer(model_name, losses, examples, buffer):
+    samples = examples[0]
+    predictions = examples[1]
+    labels = examples[2]
 
-#   print(f"Saving model {model_name}")
-#   with open('results_zero_shot.txt', 'a') as f:
-#       f.write("-" * 112 + "\n")  # Seperator
-#       f.write(f"Model name: {model_name}\n")
-#       f.write(f"R2 Squard loss: {losses[0]}\n")
-#       f.write(f"MSE loss: {losses[1]}\n")
-#       f.write(f"MAE loss: {losses[2]}\n\n")
+    buffer += "-" * 112 + "\n"  # Seperator
+    buffer += f"Model name: {model_name}\n"
+    buffer += f"R2 Squard loss: {losses[0]}\n"
+    buffer += f"MSE loss: {losses[1]}\n"
+    buffer += f"MAE loss: {losses[2]}\n\n"
 
-#       for i in range(N_EXAMPLE_SAMPLES):
-#           f.write(f"Sample {i}:\n{samples[i]}\n\n")
-#           f.write(f"Price prediction:\n{predictions[i]}\n")
-#           f.write(f"True price:\n{labels[i]}\n")
-#           f.write("-" * 112 + "\n")
-#       print(f"Saving model {model_name} 2")
-
-def save_to_buffer(model_name, losses, samples, predictions, labels,buffer):
-  buffer += "-" * 112 + "\n"  # Seperator
-  buffer +=f"Model name: {model_name}\n"
-  buffer +=f"R2 Squard loss: {losses[0]}\n"
-  buffer +=f"MSE loss: {losses[1]}\n"
-  buffer +=f"MAE loss: {losses[2]}\n\n"
-
-  for i in range(N_EXAMPLE_SAMPLES):
-    buffer +=f"Sample {i}:\n{samples[i]}\n\n"
-    buffer +=f"Price prediction:\n{predictions[i]}\n"
-    buffer +=f"True price:\n{labels[i]}\n"
-    buffer +="-" * 112 + "\n"
-  return buffer
-
+    for i in range(N_EXAMPLE_SAMPLES):
+        buffer += f"Sample {i}:\n{samples[i]}\n\n"
+        buffer += f"Price prediction:\n{predictions[i]}\n"
+        buffer += f"True price:\n{labels[i]}\n"
+        buffer += "-" * 112 + "\n"
+    return buffer
 
 
 def save_results(buffer):
-  with open('results_zero_shot.txt', 'w') as f:
-    f.write(buffer)
+    with open('results_zero_shot.txt', 'w') as f:
+        f.write(buffer)
 
 
 def evaluate(predictions, labels):
@@ -90,8 +83,6 @@ def evaluate(predictions, labels):
     mae = mae_metric.compute(predictions=predictions, references=labels)['mae']
 
     return [r_squared, mse, mae]
-
-
 
 
 def zero_shot(model_name, model_for_lm, mask_format, train_data):
@@ -109,7 +100,7 @@ def zero_shot(model_name, model_for_lm, mask_format, train_data):
     truncated_masked_data_sentences = tokenizer.batch_decode(
         tokenizer(masked_data_sentences, max_length=overview_truncation_len, truncation=True)['input_ids'])
 
-    truncated_formatted_sentences = [s+added_format for s in truncated_masked_data_sentences]
+    truncated_formatted_sentences = [s + added_format for s in truncated_masked_data_sentences]
     inputs = tokenizer(truncated_formatted_sentences, return_tensors="pt", truncation=True, padding=True)
 
     with torch.no_grad():
@@ -118,7 +109,7 @@ def zero_shot(model_name, model_for_lm, mask_format, train_data):
 
     max_val = torch.max(logits)
     mask_token_indexs = (inputs.input_ids == tokenizer.mask_token_id).nonzero(as_tuple=True)
-    numerical_ids = tokenizer.encode("0 1 2 3 4 5 6 7 8 9", add_special_tokens=False)
+    numerical_ids = get_numerical_tokens(tokenizer)
 
     # maximazing only the numerical token to only them will get picked
     for indx in numerical_ids:
@@ -127,26 +118,36 @@ def zero_shot(model_name, model_for_lm, mask_format, train_data):
 
     predicted_token_id = logits[mask_token_indexs[0], mask_token_indexs[1]].argmax(axis=-1)
     predicted_words = tokenizer.batch_decode(predicted_token_id)
-    print(f"Predicted words: {predicted_words}")
     predicted_prices = predictions_to_numbers(predicted_words)
 
     losses = evaluate(predicted_prices, true_data_completion)
 
-    return losses, truncated_formatted_sentences , true_data_completion , predicted_prices
+    return losses, truncated_formatted_sentences, true_data_completion, predicted_prices
 
 
-
-
-if __name__ == '__main__':
+def main():
     # Uncomment if there are not csv files saved
     # df_train, df_test = build_df_from_data()
 
     buffer = ""
     for model_name, model_for_lm, mask_format in MODELS:
-        train_data = format_dataframe("train_data.csv", "{overview}")[:SLICE_DATA_FOR_DEBUG]
-        losses, truncated_formatted_sentences , true_data_completion , predicted_prices = zero_shot(model_name, model_for_lm, mask_format, train_data)
-
-        buffer = save_to_buffer(model_name, losses, truncated_formatted_sentences[:N_EXAMPLE_SAMPLES],
-              predicted_prices[:N_EXAMPLE_SAMPLES],
-              true_data_completion[:N_EXAMPLE_SAMPLES],buffer)
+        train_data = format_dataframe("train_data.csv", "{overview}")
+        avg_losses = [0, 0, 0]
+        examples = []
+        for i in range(0, len(train_data), BATCH_SIZE):
+            losses, truncated_formatted_sentences, true_data_completion, predicted_prices = zero_shot(
+                model_name, model_for_lm, mask_format, train_data[i:i + BATCH_SIZE])
+            if i == 0:
+                examples = [truncated_formatted_sentences[:N_EXAMPLE_SAMPLES],
+                            predicted_prices[:N_EXAMPLE_SAMPLES],
+                            true_data_completion[:N_EXAMPLE_SAMPLES]]
+            avg_losses[0] += losses[0]
+            avg_losses[1] += losses[1]
+            avg_losses[2] += losses[2]
+        num_batches = ceil(len(train_data) / BATCH_SIZE)
+        buffer = save_to_buffer(model_name, [avg / num_batches for avg in avg_losses], examples, buffer)
     save_results(buffer)
+
+
+if __name__ == '__main__':
+    main()
