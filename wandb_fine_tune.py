10c582cfc58231ba35810f2926c81140485b1021
avishaiElmakies <36810152+avishaiElmakies@users.noreply.github.com>
2023-07-19 15:55:55 +0300

    update sweep

--- wandb_fine_tune.py
+++ wandb_fine_tune.py
@@ -12,8 +12,9 @@ def get_time():
     return now.strftime("%d-%m-%Y__%H-%M-%S")
 
 
-def get_config(augment, del_p):
+def get_config(name, augment, del_p):
     sweep_config = {'method': 'random',
+                    'name': name,
                     'metric': {'name': 'eval/mse', 'goal': 'minimize'}}
 
     param_dict = {
@@ -60,26 +61,30 @@ def wandb_run(config=None):
 
 
 def main():
-    # args = argparse.ArgumentParser()
-    # args.add_argument("--augment", default=False, help="use augmented data")
-    # args.add_argument("--del_p", default=0.1, help="probability to delete")
-    #
-    # args = args.parse_args()
-    # sweep_config = get_config(args.augment, args.del_p)
-    # sweep_id = wandb.sweep(sweep_config, project="anlp_project",
-    #                        entity="selling_bat_yam",)
-    # wandb.agent(sweep_id, wandb_run, count=1000)
-    config = {
-        'model_name': 'bert-base-uncased',
-        'epoch': 1,
-        'learning_rate': 0.0001,
-        'weight_decay': 0.0,
-        'beta': 0.0,
-        'augment': False,
-        'del_p': 0.1
-    }
+    args = argparse.ArgumentParser()
+    args.add_argument("--sweep_name",type=str)
+    args.add_argument("--augment", default=False, type=lambda x: x == "True",
+                      help="use augmented data")
+    args.add_argument("--del_p", default=0.1, type=float, help="probability to "
+                                                               "delete")
 
-    wandb_run(config)
+    args = args.parse_args()
+    sweep_config = get_config(args.sweep_name,args.augment, args.del_p)
+    sweep_id = wandb.sweep(sweep_config, project="anlp_project",
+                           entity="selling_bat_yam")
+    print(sweep_id)
+    wandb.agent(sweep_id, wandb_run, count=1000)
+    # config = {
+    #     'model_name': 'bert-base-uncased',
+    #     'epoch': 1,
+    #     'learning_rate': 0.0001,
+    #     'weight_decay': 0.0,
+    #     'beta': 0.0,
+    #     'augment': False,
+    #     'del_p': 0.1
+    # }
+    #
+    # wandb_run(config)
 
 
 if __name__ == '__main__':
