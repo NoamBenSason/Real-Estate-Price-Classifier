ab9fa5fdb3bcd01fb57f14604a9e14add0ac2721
avishaiElmakies <36810152+avishaiElmakies@users.noreply.github.com>
2023-07-23 16:45:02 +0300

    bug fix

--- evaluate_models.py
+++ evaluate_models.py
@@ -9,7 +9,7 @@ from datasets import Dataset
 from fine_tuning import fine_tune_model, tokenize_func, convert_data
 
 SPECIAL_TOKENS = ['[bd]', '[br]', '[address]', '[overview]', '[sqft]']
-MODELS = ['bert-base-uncased', 'roberta-base', 'google/electra-base-generator'
+MODELS = ['bert-base-uncased', 'roberta-base', 'google/electra-base-generator',
           'bert-large-uncased', 'roberta-large',
           'google/electra-large-generator'
           ]
